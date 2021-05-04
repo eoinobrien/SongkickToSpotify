ccf8b05ce8aadc9e5aaf401ed5e507d14c054185
Eoin O'Brien <eoin@eoinobrien.ie>
2021-05-04 20:57:22 +0100

    update site styles

--- site/src/components/Playlist/Playlist.tsx
+++ site/src/components/Playlist/Playlist.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from 'next/image';
 import { PlaylistComponentProps } from '.';
 import styles from './Playlist.module.css';
 
@@ -7,22 +7,25 @@ export const Playlist: React.FC<PlaylistComponentProps> = (
 ) => {
   return (
     <div className={styles.playlist}>
-      <Image
-        src={'/cover-art/' + props.playlist.PlaylistTitle + '.jpg'}
-        alt={'Playlist cover art for ' + props.playlist.PlaylistTitle + '.'}
-        width={200}
-        height={200}
-        className={styles.coverArt}
-      />
       <a
         href={'https://open.spotify.com/playlist/' + props.playlist.PlaylistId}
         target="_blank"
+        className={styles.spotify}
       >
-        Play on Spotify
+        <Image
+          src={'/cover-art/' + props.playlist.PlaylistTitle + '.jpg'}
+          alt={'Playlist cover art for ' + props.playlist.PlaylistTitle + '.'}
+          width={200}
+          height={200}
+          className={styles.coverArt}
+        />
+        <h2 className={styles.spotifyButton}>Play on Spotify</h2>
       </a>
       <a
         href={'https://www.songkick.com/metro-areas/' + props.playlist.MetroId}
         target="_blank"
+
+        className={styles.songkick}
       >
         See concerts on Songkick
       </a>
--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -5,30 +5,38 @@ import { Playlists } from '../components/Playlists';
 import { GetStaticProps } from 'next';
 import { PlaylistType } from '@/components/Playlist';
 
-export default function Home({playlists}: IndexPageProps) {
+export default function Home({ playlists }: IndexPageProps) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Playlists for Upcoming Concerts</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Upcoming <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Upcoming</h1>
 
-        <Playlists playlists={playlists}/>
+        <p  className={styles.description}>Each playlist contains the top songs for artists who are performing in that city in the coming period. Playlists are updated daily.</p>
+
+        <Playlists playlists={playlists} />
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://eoinobrien.ie"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Created by Eoin O'Brien
-        </a>
+        <div>
+          <p>
+            Playlists are updated daily using Concert data from{' '}
+            <a href="https://www.songkick.com">Songkick</a>.
+          </p>
+        </div>
+        <div>
+          <a
+            href="https://eoinobrien.ie"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Created by Eoin O'Brien
+          </a>
+        </div>
       </footer>
     </div>
   );
