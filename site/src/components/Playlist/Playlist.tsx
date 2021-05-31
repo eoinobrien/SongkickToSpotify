2643d34221615a4b1801a7e93dc2b7f3e50fe4b5
Eoin O'Brien <eoin@eoinobrien.ie>
2021-05-31 22:58:26 +0100

    feat: change card to alter layout at certain width

--- site/src/components/Playlist/Playlist.tsx
+++ site/src/components/Playlist/Playlist.tsx
@@ -23,51 +23,55 @@ export const Playlist: React.FC<PlaylistComponentProps> = ({
 
   return (
     <div className={styles.playlist}>
-      <a
-        href={`https://open.spotify.com/playlist/${playlist.PlaylistId}`}
-        target="_blank"
-        className={styles.spotify}
-        rel="noreferrer"
-      >
-        <picture>
-          <source
-            // eslint-disable-next-line import/no-dynamic-require, global-require
-            srcSet={require(`./cover-art/${imagePath(
-              playlist.PlaylistType,
-              playlist.Area,
-            )}?webp`)}
-            type="image/webp"
-          />
-          <source
-            // eslint-disable-next-line import/no-dynamic-require, global-require
-            srcSet={require(`./cover-art/${imagePath(
-              playlist.PlaylistType,
-              playlist.Area,
-            )}`)}
-            type="image/jpeg"
-          />
-          <img
-            // eslint-disable-next-line import/no-dynamic-require, global-require
-            src={require(`./cover-art/${imagePath(
-              playlist.PlaylistType,
-              playlist.Area,
-            )}`)}
-            alt={playlist.PlaylistTitle}
-            className={styles.coverArt}
-            width="200"
-            height="200"
-          />
-        </picture>
-        <h2 className={styles.spotifyButton}>Play on Spotify</h2>
-      </a>
-      <a
-        href={`https://www.songkick.com/metro-areas/${playlist.MetroId}`}
-        target="_blank"
-        className={styles.songkick}
-        rel="noreferrer"
-      >
-        See concerts on Songkick
-      </a>
+      <picture title={`${playlist.PlaylistTitle}`}>
+        <source
+          // eslint-disable-next-line import/no-dynamic-require, global-require
+          srcSet={require(`./cover-art/${imagePath(
+            playlist.PlaylistType,
+            playlist.Area,
+          )}?webp`)}
+          type="image/webp"
+        />
+        <source
+          // eslint-disable-next-line import/no-dynamic-require, global-require
+          srcSet={require(`./cover-art/${imagePath(
+            playlist.PlaylistType,
+            playlist.Area,
+          )}`)}
+          type="image/jpeg"
+        />
+        <img
+          // eslint-disable-next-line import/no-dynamic-require, global-require
+          src={require(`./cover-art/${imagePath(
+            playlist.PlaylistType,
+            playlist.Area,
+          )}`)}
+          alt={playlist.PlaylistTitle}
+          className={styles.coverArt}
+          width="200"
+          height="200"
+        />
+      </picture>
+
+      <div className={styles.links}>
+        <a
+          href={`https://open.spotify.com/playlist/${playlist.PlaylistId}`}
+          target="_blank"
+          className={styles.spotify}
+          rel="noreferrer"
+        >
+          <h2 className={styles.spotifyButton}>Play on Spotify</h2>
+        </a>
+        <a
+          href={`https://www.songkick.com/metro-areas/${playlist.MetroId}`}
+          target="_blank"
+          className={styles.songkick}
+          rel="noreferrer"
+          title={`See upcoming concerts in ${playlist.Area} on Songkick`}
+        >
+          See concerts on Songkick
+        </a>
+      </div>
     </div>
   );
 };
