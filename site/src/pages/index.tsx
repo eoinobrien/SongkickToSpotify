7e74bbde46f7e43d98df0ad8fcf01b97fa8c1e12
Eoin O'Brien <eoin@eoinobrien.ie>
2021-05-31 23:22:45 +0100

    feat: add citybreezesounds source

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { GetStaticProps } from 'next';
 import { PlaylistCity } from '@/components/Playlist';
 import { FilterablePlaylists } from '../components/FilterablePlaylists';
@@ -7,14 +8,25 @@ import styles from '../styles/Home.module.css';
 const playlistsJson = require(`./Playlists.json`);
 
 export default function Home({ playlists }: IndexPageProps) {
+  const router = useRouter();
+  const { source } = router.query;
+
+  const cityBreezeSounds =
+    source?.toString().toLowerCase() === `citybreezesounds`;
+
   return (
-    <div className={styles.container}>
+    <div
+      className={`${styles.container} ${
+        cityBreezeSounds && styles.cityBreezeSounds
+      }`}
+    >
       <Head>
         <title>Playlists for Upcoming Concerts</title>
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>Upcoming</h1>
+        {cityBreezeSounds && <h2>City Breeze Sounds!</h2>}
 
         <p className={styles.description}>
           Each playlist contains the top songs for artists who are performing in
