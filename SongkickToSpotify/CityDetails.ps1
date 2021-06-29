fe70fa4a24a603402e46b0598fcdbd3587e59b00
Eoin O'Brien <eoin@eoinobrien.ie>
2021-06-29 22:12:27 +0100

    fix: improve city details script

--- SongkickToSpotify/CityDetails.ps1
+++ SongkickToSpotify/CityDetails.ps1
@@ -1,28 +1,40 @@
-. $PSScriptRoot\Common.ps1
-. $PSScriptRoot\Spotify.ps1
-. $PSScriptRoot\Songkick.ps1
-. $PSScriptRoot\SpotifyAuth.ps1
+Import-Module $PSScriptRoot\Modules\Common.psm1
+Import-Module $PSScriptRoot\Modules\Spotify.psm1
+Import-Module $PSScriptRoot\Modules\Songkick.psm1
+Import-Module $PSScriptRoot\Modules\SpotifyAuth.psm1
 
+
+function Add-CommaToString($string)
+{
+	return $string ? "$string, " : '';
+}
 
 $SongkickAPIKey = ""
 $SpotifyKey = "Bearer $(Refresh-AuthToken)"
 
-$cities = @("Milan");
+$cities = @(
+	@{
+		City    = "Milan"
+		Region    = ""
+		Country = "Italy"
+	}
+);
 
 $playlists = @()
 
 
-foreach ($city in $cities)
-{
-	$locations = Search-Locations $city
+foreach ($city in $cities) {
+	$cityName = ("$(Add-CommaToString $city.City)$(Add-CommaToString $city.Region)$(Add-CommaToString $city.Country)").TrimEnd(", ")
+
+	$locations = Search-Locations $cityName
 
 	$playlist = @{
-		"PlaylistId" = "a"
-		"PlaylistTitle" = "Upcoming $city"
-		"Area" = $city
-		"MetroId" = $locations[0].metroArea.id
-		"PhotoCredit" = "a"
-		"Offset" = 14
+		"PlaylistId"    = "a"
+		"PlaylistTitle" = "Upcoming $city.City"
+		"Area"          = $city
+		"MetroId"       = $locations[0].metroArea.id
+		"PhotoCredit"   = "a"
+		"Offset"        = 14
 	}
 
 	$spotifyPlaylist = New-Playlist $playlist.PlaylistTitle
